fea0124e830a02989d376a48ae131e4b239d13b7
Metalheadpnz <metalheadpnz@gmail.com>
2022-03-10 18:59:03 +0300

    Updates

---
+++ precache-manifest.9321957272a4836536aa75f2c6b9a3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14d1f4fdca77b74e608a701c4625904",
+    "url": "/ignatHW/index.html"
+  },
+  {
+    "revision": "225100aa32beac191917",
+    "url": "/ignatHW/static/css/main.329c7148.chunk.css"
+  },
+  {
+    "revision": "7cf06d1cae2e467916d9",
+    "url": "/ignatHW/static/js/2.98a0ea15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ignatHW/static/js/2.98a0ea15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225100aa32beac191917",
+    "url": "/ignatHW/static/js/main.6cfa4dd8.chunk.js"
+  },
+  {
+    "revision": "b2b677c63ac4cc85bb59",
+    "url": "/ignatHW/static/js/runtime-main.f5e30dcd.js"
+  }
+]);
