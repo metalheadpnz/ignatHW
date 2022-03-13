38e97562dbe60d1fc2489906506ee146fb1dfbac
Metalheadpnz <metalheadpnz@gmail.com>
2022-03-13 18:32:54 +0300

    HW05 with HashRouter

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
-import {HashRouter} from "react-router-dom";
+import Pages from './Pages'
+import {BrowserRouter,HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
+            {/*<HashRouter>*/}
             <HashRouter>
+                <Header/>
 
-            <Header/>
-
-            <Routes/>
-
+                <Pages/>
             </HashRouter>
+            {/*</HashRouter>*/}
         </div>
     )
 }
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div>
             // add NavLinks
-
+            <NavLink to={'/test'}><div>testLInk</div></NavLink>
+            <NavLink to={'/'}><div>Home</div></NavLink>
         </div>
     )
 }
