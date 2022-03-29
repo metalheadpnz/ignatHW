e30ab9c4ff4ed35cf82856de651a216b59e8d453
Metalheadpnz <metalheadpnz@gmail.com>
2022-03-29 13:40:22 +0300

    HW07 done test deploy

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
-import {BrowserRouter,HashRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
             {/*<HashRouter>*/}
+            test deploy
             <HashRouter>
                 <Header/>
 
